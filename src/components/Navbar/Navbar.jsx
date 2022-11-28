80730aa5a22008b6112840029469bc94b910ddf2
Md. Sakhawat Hossain <biplobbms@gmail.com>
2022-11-28 18:23:51 +0600

    Added Styled Component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+const Container = styled.div`
+height: 60px;
+
+`
+const Wrapper = styled.div`
+padding: 10px 20px;
+display: flex;
+`
+const Navbar = () => {
+  return (
+    <Container>Navbar</Container>
+  )
+}
+
+export default Navbar
